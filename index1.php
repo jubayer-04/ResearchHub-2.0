9e57bcbdd28a03cf026f4d408bbc2f6926f98b52
Jubayer Hossain <jubayerhr@gmail.com>
2026-01-14 23:40:57 +0600

    Update index1.php

--- index1.php
+++ index1.php
@@ -65,6 +65,7 @@ $year_stmt->close();
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ResearchHub</title>
     <link rel="stylesheet" href="styles.css">
